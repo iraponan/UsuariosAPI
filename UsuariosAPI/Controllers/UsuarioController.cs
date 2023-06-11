88d7ddf5e3caaf7e4ba93f31039618ec41c711c0
Iraponan Marinho <iraponan.aires@gmail.com>
2023-06-11 17:07:13 -0300

    Concluindo o cadastro do usuario.

--- UsuariosAPI/Controllers/UsuarioController.cs
+++ UsuariosAPI/Controllers/UsuarioController.cs
@@ -1,14 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using UsuariosAPI.Data.Dtos;
+using UsuariosAPI.Models;
 
 namespace UsuariosAPI.Controllers {
 
     [ApiController]
     [Route("[Controller]")]
     public class UsuarioController : ControllerBase {
+
+        private IMapper _mapper;
+        private UserManager<Usuario> _userManager;
+
+        public UsuarioController(IMapper mapper, UserManager<Usuario> userManager) {
+            _mapper = mapper;
+            _userManager = userManager;
+        }
+
         [HttpPost]
-        public IActionResult CadastrarUsuario(CreateUsuarioDto dto) {
-            throw new NotImplementedException();
+        public async Task<IActionResult> CadastrarUsuario(CreateUsuarioDto dto) {
+            Usuario usuario = _mapper.Map<Usuario>(dto);
+            IdentityResult resultado = await _userManager.CreateAsync(usuario, dto.Senha);
+            if (resultado.Succeeded) {
+                return Ok("Usuário " + dto.UserName + " Cadastrado!");
+            }
+
+            throw new ApplicationException("Falha ao cadastrar usuário!");
         }
     }
 }
