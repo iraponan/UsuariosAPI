7afd9a00de922c50e5f011973ec4862ca8ed1c60
Iraponan Marinho <iraponan.aires@gmail.com>
2023-06-11 16:24:08 -0300

    Configurando o banco de dados.

--- UsuariosAPI/Models/Usuario.cs
+++ UsuariosAPI/Models/Usuario.cs
@@ -1,4 +1,9 @@
-﻿namespace UsuariosAPI.Models {
-    public class Usuario {
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace UsuariosAPI.Models {
+    public class Usuario : IdentityUser {
+        public DateTime DataNascimento { get; set; }
+
+        public Usuario() : base() { }
     }
 }
--- UsuariosAPI/Program.cs
+++ UsuariosAPI/Program.cs
@@ -1,6 +1,22 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using UsuariosAPI.Data;
+using UsuariosAPI.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+var connectionString = builder.Configuration.GetConnectionString("UsuarioConnection");
+
+builder.Services.AddDbContext<UsuarioDbContext>(opts => {
+    opts.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+    }) ;
+
+builder.Services
+    .AddIdentity<Usuario, IdentityRole>()
+    .AddEntityFrameworkStores<UsuarioDbContext>()
+    .AddDefaultTokenProviders();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
