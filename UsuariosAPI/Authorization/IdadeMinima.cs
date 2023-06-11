f5fa43ca3423e2bb2d4db35b6d9699b6cfdab95e
Iraponan Marinho <iraponan.aires@gmail.com>
2023-06-11 18:25:40 -0300

    Criando politicas de acesso.

---
+++ UsuariosAPI/Authorization/IdadeMinima.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace UsuariosAPI.Authorization {
+    public class IdadeMinima : IAuthorizationRequirement {
+        public int idade { get; set; }
+
+        public IdadeMinima(int idade) {
+            this.idade = idade;
+        }
+    }
+}
--- UsuariosAPI/Program.cs
+++ UsuariosAPI/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using UsuariosAPI.Authorization;
 using UsuariosAPI.Data;
 using UsuariosAPI.Models;
 using UsuariosAPI.Services;
@@ -28,6 +29,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthorization(options => {
+    options.AddPolicy("IdadeMinima", policy => policy.AddRequirements(new IdadeMinima(18)));
+});
 
 var app = builder.Build();
 
